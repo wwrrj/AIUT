b2a4bf0466fb2761f5adc391c613915beb85ef6e
“wwrrj” <2858594753@qq.com>
2021-07-18 19:35:59 +0800

    Add extract Payload.bin function

--- menu.sh
+++ menu.sh
@@ -9,11 +9,11 @@ fi
 }
 
 Check bin
-if [[ $Result = 0 ]];then
+if [[ $Result == 0 ]];then
     echo "./Bin not found,Download!"
     wget https://hub.fastgit.org/wwrrj/AIUT/releases/download/v1.1/bin.zip > /dev/null 2>&1
     Check bin.zip
-    if [[ $Result = 1 ]];then
+    if [[ $Result == 1 ]];then
         unzip -d ./bin bin.zip
         rm -rf binary.zip
         clear
@@ -25,6 +25,21 @@ if [[ $Result = 0 ]];then
     fi
 fi
 
+Info(){
+    echo "----------------------------------"
+    echo "The last run result: " 
+    cat ./bin/.create
+    Check IMG/boot/imageinfo.txt
+    if [[ $Result == 1 ]];then
+        echo "----------------------------------"
+        echo "Kernel info: "
+        cat IMG/boot/imageinfo.txt | grep BOARD
+        echo "----------------------------------"
+        echo "[boot.img/recovery.img] --> IMG/boot"
+    fi
+    echo "----------------------------------"
+}
+
 Menu(){
     echo "----------------------------------"
     echo "一.分解专区"
@@ -35,17 +50,7 @@ Menu(){
     echo "  5.分解system.img"
     echo "  6.分解vendor.img"
     echo "  7.分解boot/recovery.img"
-    echo "----------------------------------"
-    echo "The last run result: " 
-    cat ./bin/.create
-    Check IMG/boot/imageinfo.txt
-    if [[ $Result = 1 ]];then
-        echo "----------------------------------"
-        echo "Kernel info: "
-        cat IMG/boot/imageinfo.txt | grep BOARD
-        echo "----------------------------------"
-        echo "[boot.img/recovery.img] --> IMG/boot"
-    fi
+    echo "  8.分解Payload.bin"
     echo "----------------------------------"
 }
 
@@ -64,10 +69,10 @@ Clean(){
 
 UnpackBr(){
     Check Input/$1.new.dat
-    if [[ $Result = 0 ]];then
+    if [[ $Result == 0 ]];then
         ./bin/brotli -d Input/$1.new.dat.br
         Check Input/system.new.dat
-        if [[ $Result = 1 ]];then
+        if [[ $Result == 1 ]];then
             echo "Done!!!!!!!" > ./bin/.create
          else
             echo "Fuck!!!Where's my $1.new.dat?!!" > ./bin/.create
@@ -80,12 +85,12 @@ UnpackBr(){
 
 UnpackDat(){
     Check Input/$1.img
-    if [[ $Result = 0 ]];then
+    if [[ $Result == 0 ]];then
         Check Input/$1.transfer.list
-        if [[ $Result = 1 ]];then
+        if [[ $Result == 1 ]];then
             python ./bin/sdat2img.py ./Input/$1.transfer.list ./Input/$1.new.dat ./Input/$1.img | grep Done
             Check Input/system.img
-            if [[ $Result = 1 ]];then
+            if [[ $Result == 1 ]];then
                 echo "Done!!!!!!!" > ./bin/.create
             else
                 echo "Fuck!!!Where's my $1.img?!!" > ./bin/.create
@@ -101,10 +106,10 @@ UnpackDat(){
 
 UnpackIMG(){
     Check Input/$1
-    if [[ $Result = 0 ]];then
+    if [[ $Result == 0 ]];then
         python3 ./bin/imgextractor.py ./Input/$1.img ./Input/
         Check Input/$1
-        if [[ $Result = 1 ]];then
+        if [[ $Result == 1 ]];then
             echo "Done!!!!!!!" > ./bin/.create
         else
             echo "Fuck!!!Where's my $1?!!" > ./bin/.create
@@ -116,9 +121,9 @@ UnpackIMG(){
 
 Kernel(){
     bash bin/AIK-Linux/cleanup.sh
-    if [[ $1 = unpack ]];then
+    if [[ $1 == unpack ]];then
         Check IMG/boot
-        if [[ $Result = 1 ]];then
+        if [[ $Result == 1 ]];then
             rm -rf IMG/boot
             mkdir IMG/boot
         else
@@ -127,117 +132,137 @@ Kernel(){
         fi        
         bash bin/AIK-Linux/unpackimg.sh > IMG/boot/imageinfo.txt
         Check bin/AIK-Linux/split_img
-        if [[ $Result = 1 ]];then
+        if [[ $Result == 1 ]];then
             mv bin/AIK-Linux/split_img IMG/boot
             mv bin/AIK-Linux/ramdisk IMG/boot
             echo "Done!!!!!!!" > ./bin/.create
         else
             echo "Fuck!!!Where's my $1?!!" > ./bin/.create
         fi
-    elif [[ $1 = repack ]];then
+    elif [[ $1 == repack ]];then
         bash bin/AIK-Linux/repackimg.sh  > /dev/null 2>&1
     fi
 }
 
+Payload(){
+    python bin/extract_android_ota_payload.py Input/payload.bin Payload
+    Check Payload/system.img
+    if [[ $Result == 1 ]];then
+        echo "Input/payload.bin --> Payload" > ./bin/.create
+    else
+        echo "Input/payload.bin extracted failed!!!" > ./bin/.create
+    fi
+}
+
 Check './bin/.create' 
-if [[ $Result = "0" ]];then
+if [[ $Result == "0" ]];then
     mkdir Input
     mkdir IMG
+    mkdir Payload
     touch ./bin/.create
 fi
 
-if [[ $1 = "--m" ]]; then 
+if [[ $1 == "--m" ]]; then 
     Menu
     read -p "Choose: " Choose
-elif [[ $1 = "--c" ]]; then
+elif [[ $1 == "--c" ]]; then
     read -p "Are you sure you want to clean up work path?[y(es)/n(o)]" yn
     if [[ $yn == y ]];then
         Clean
-        Menu
+        Info
     elif [[ $yn == yes ]];then
         Clean
-        Menu
+        Info
     elif [[ $yn == n ]];then
-        echo "OK, return to the menu" > ./bin/.create
-        Menu
+        echo "OK!" > ./bin/.create
+        Info
     elif [[ $yn == no ]];then
-        echo "OK, return to the menu" > ./bin/.create
-        Menu
+        echo "OK!" > ./bin/.create
+        Info
     fi
 else
     Usage
 fi
 
 
-if [[ $Choose = "1" ]];then
+if [[ $Choose == "1" ]];then
     Check Input/system.new.dat.br
-    if [[ $Result = 1 ]];then
+    if [[ $Result == 1 ]];then
         UnpackBr system
-        Menu
+        Info
     else
         echo "Input/[system.new.dat.br] not found!!!!!!" > ./bin/.create
-        Menu        
+        Info
     fi
-elif [[ $Choose = "2" ]];then
+elif [[ $Choose == "2" ]];then
     Check Input/system.new.dat
-    if [[ $Result = 1 ]];then
+    if [[ $Result == 1 ]];then
         UnpackDat system
-        Menu
+        Info
     else
         echo "Input/[system.new.dat] not found!!!!!!" > ./bin/.create
-        Menu
+        Info
     fi
-elif [[ $Choose = "3" ]];then
+elif [[ $Choose == "3" ]];then
     Check Input/vendor.new.dat.br
-    if [[ $Result = 1 ]];then
+    if [[ $Result == 1 ]];then
         UnpackBr vendor
-        Menu
+        Info
     else
         echo "Input/[vendor.new.dat.br] not found!!!!!!" > ./bin/.create
-        Menu
+        Info
     fi
-elif [[ $Choose = "4" ]];then
+elif [[ $Choose == "4" ]];then
     Check Input/vendor.new.dat
-    if [[ $Result = 1 ]];then
+    if [[ $Result == 1 ]];then
         UnpackBr vendor
-        Menu
+        Info
     else
         echo "Input/[vendor.new.dat] not found!!!!!!" > ./bin/.create
-        Menu
+        Info
     fi
-elif [[ $Choose = "5" ]];then
+elif [[ $Choose == "5" ]];then
     Check Input/system.img
-    if [[ $Result = 1 ]];then
+    if [[ $Result == 1 ]];then
         UnpackIMG system
-        Menu
+        Info
     else
         echo "Input/[system.img] found!!!!!!" > ./bin/.create
-        Menu
+        Info
     fi
-elif [[ $Choose = "6" ]];then
+elif [[ $Choose == "6" ]];then
     Check Input/vendor.img
-    if [[ $Result = 1 ]];then
+    if [[ $Result == 1 ]];then
         UnpackIMG vendor
-        Menu
+        Info
     else
         echo "Input/[vendor.img] not found!!!!!!" > ./bin/.create
-        Menu
+        Info
     fi
-elif [[ $Choose = "7" ]];then
+elif [[ $Choose == "7" ]];then
     Check Input/boot.img
-    if [[ $Result = 1 ]];then
+    if [[ $Result == 1 ]];then
         mv Input/boot.img bin/AIK-Linux
         Kernel unpack
-        Menu
-    elif [[ $Result = 0 ]];then
+        Info
+    elif [[ $Result == 0 ]];then
         Check Input/recovery.img
-        if [[ $Result = 1 ]];then
+        if [[ $Result == 1 ]];then
             mv Input/recovery.img bin/AIK-Linux
             Kernel unpack
-            Menu
+            Info
         else
             echo "Input/[boot.img/recovery.img] not found!!!!!!" > ./bin/.create
-            Menu        
+            Info     
         fi
-    fi  
+    fi
+elif [[ $Choose == "8" ]];then
+    Check Input/payload.bin
+    if [[ $Result == 1 ]];then
+        Payload
+        Info
+    elif [[ $Result == 0 ]];then
+        echo "Input/payload.bin not found!!!!!!" > ./bin/.create
+        Info
+    fi
 fi
